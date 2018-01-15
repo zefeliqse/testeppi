7a421b2159905364b1f57eb6587f49220dbf55f3
José Feliqs <felix.silva1337@outlook.com>
2018-01-15 17:03:02 -0300

    Building the first servlet.

---
+++ testegit/src/testegit/testeServlet.java
@@ -0,0 +1,25 @@
+package testegit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet ("/oi")
+public class testeServlet extends HttpServlet  {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+	PrintWriter out = response.getWriter();
+	out.println("<html>");
+	out.println("<body>");
+	out.println("<h1> OI </h1>");
+	out.println("</body>");
+	out.println("</html>");	
+	}
+	
+
+}
